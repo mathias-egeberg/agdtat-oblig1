2a4e9255211bf6ae4b70b7f3d9e93e6b7fafbefe
Mathais <mathias.egeberg@hotmail.no>
2024-09-03 10:52:59 +0200

    Oppgave 0 og oppgave 1

--- src/Oblig1.java
+++ src/Oblig1.java
@@ -1,6 +1,14 @@
 public class Oblig1 {
 
+    //Oppgave 0
     public static int gruppeMedlemmer(){
         return 2;
     }
+
+
+    //Oppgave 1
+    public static int maks(int[] a){
+        return 0;
+    }
+
 }
